d5bc7946587799b032f458923d8c5984c26ab2b6
ketan295 <130463504+ketan295@users.noreply.github.com>
2023-04-13 18:24:50 +0530

    Add files via upload

---
+++ GN1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main()
+
+{
+	
+	printf("\t *         *\n");                     
+	printf("     *        *\n");
+    printf("*\t *\n");
+    printf("     *        *\n");
+    printf("\t *         *\n");
+    printf("\n");
+    printf("\n");
+    printf("           (I)");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    
+    
+  	printf("\t*   *   *   *   *   *   *");
+  	printf("\n\n\n");
+  	printf("  *\t*   *   *   *   *   *   *\n\n");
+  	printf("  *         *                   *\n\n");
+  	printf("  *\t    *       *   *   *   *\n\n");
+  	printf("  *\t    *       *       *   *\n\n");
+  	printf("  *\t*   *   *   *   *   *   *");
+  	printf("\n");
+    printf("\n");
+    printf("           (II)");
+  
+  	
+    
+    
+    
+
+	
+}
+
